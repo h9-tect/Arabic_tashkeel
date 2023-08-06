8ed480abf49aabb7e0c7c720e033bd4ace2c5ed4
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-08-06 03:54:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import mishkal.tashkeel
-from spellchecker import SpellChecker
+from ghalatawi.autocorrector import AutoCorrector
 
-spell = SpellChecker(language='ar')
+autoco = AutoCorrector()
 
 def tashkeel_app(text):
     vocalizer = mishkal.tashkeel.TashkeelClass()
@@ -10,25 +10,31 @@ def tashkeel_app(text):
     return tashkeeled_text
 
 def correct_spelling(text):
-    words = text.split()
-    corrected_words = [spell.correction(word) for word in words]
-    corrected_text = ' '.join(corrected_words)
-    return corrected_text
+    return autoco.spell(text)
 
 def main():
-    st.title("Tashkeel and Spelling Corrector App")
-    st.write("This app adds Tashkeel (Arabic diacritics) and corrects spelling for the given text.")
+    st.title("Tashkeel and Spell Checker App")
+    st.write("This app adds Tashkeel (Arabic diacritics) and checks spelling for the given text.")
 
-    text_input = st.text_area("Enter your text:")
+    tashkeel_input = st.text_area("Enter your text for Tashkeel:")
 
-    if st.button("Add Tashkeel and Correct Spelling"):
-        if text_input:
-            tashkeeled_text = tashkeel_app(text_input)
-            corrected_text = correct_spelling(tashkeeled_text)
-            st.write("Tashkeeled and Corrected Text:")
+    if st.button("Add Tashkeel"):
+        if tashkeel_input:
+            tashkeeled_text = tashkeel_app(tashkeel_input)
+            st.write("Tashkeeled Text:")
+            st.write(tashkeeled_text)
+        else:
+            st.warning("Please enter some text for Tashkeel.")
+
+    spell_input = st.text_area("Enter your text for Spell Check:")
+
+    if st.button("Check Spelling"):
+        if spell_input:
+            corrected_text = correct_spelling(spell_input)
+            st.write("Corrected Text:")
             st.write(corrected_text)
         else:
-            st.warning("Please enter some text.")
+            st.warning("Please enter some text for Spell Check.")
 
 if __name__ == "__main__":
     main()
