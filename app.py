6ee1aff77342dfbae82ba81752f66448ca25f806
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-08-06 03:26:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from ar_corrector.corrector import Corrector
 import mishkal.tashkeel
 
 def tashkeel_app(text):
@@ -7,23 +6,17 @@ def tashkeel_app(text):
     tashkeeled_text = vocalizer.tashkeel(text)
     return tashkeeled_text
 
-def correct_spelling(text):
-    corr = Corrector()
-    corrected_text = corr.contextual_correct(text)
-    return corrected_text
-
 def main():
-    st.title("Tashkeel and Spelling Corrector App")
-    st.write("This app adds Tashkeel (Arabic diacritics) and corrects spelling for the given text.")
+    st.title("Tashkeel App")
+    st.write("This app adds Tashkeel (Arabic diacritics) to the given text.")
 
     text_input = st.text_area("Enter your text:")
 
-    if st.button("Add Tashkeel and Correct Spelling"):
+    if st.button("Add Tashkeel"):
         if text_input:
             tashkeeled_text = tashkeel_app(text_input)
-            corrected_text = correct_spelling(tashkeeled_text)
-            st.write("Tashkeeled and Corrected Text:")
-            st.write(corrected_text)
+            st.write("Tashkeeled Text:")
+            st.write(tashkeeled_text)
         else:
             st.warning("Please enter some text.")
 
