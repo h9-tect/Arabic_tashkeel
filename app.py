00cd608b25bd1d2e6b1ff952732c343f6b8fd059
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-08-06 04:01:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import streamlit as st
 import mishkal.tashkeel
-from ghalatawi.autocorrector import AutoCorrector
-
-autoco = AutoCorrector()
 
 def tashkeel_app(text):
     vocalizer = mishkal.tashkeel.TashkeelClass()
     tashkeeled_text = vocalizer.tashkeel(text)
     return tashkeeled_text
 
-def correct_spelling(text):
-    return autoco.spell(text)
-
 def main():
-    st.title("Tashkeel and Spell Checker App")
-    st.write("This app adds Tashkeel (Arabic diacritics) and checks spelling for the given text.")
+    st.title("Tashkeel App")
+    st.write("This app adds Tashkeel (Arabic diacritics) to the given text.")
 
     tashkeel_input = st.text_area("Enter your text for Tashkeel:")
 
@@ -25,16 +19,6 @@ def main():
             st.write(tashkeeled_text)
         else:
             st.warning("Please enter some text for Tashkeel.")
-
-    spell_input = st.text_area("Enter your text for Spell Check:")
-
-    if st.button("Check Spelling"):
-        if spell_input:
-            corrected_text = correct_spelling(spell_input)
-            st.write("Corrected Text:")
-            st.write(corrected_text)
-        else:
-            st.warning("Please enter some text for Spell Check.")
 
 if __name__ == "__main__":
     main()
